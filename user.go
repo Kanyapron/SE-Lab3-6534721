26955d772c28b4c9313b3ae1639c1717e7556d14
Kanyapron <kanyapron.kd@gmail.com>
2024-11-13 14:34:05 +0700

    close #5

--- user.go
+++ user.go
@@ -14,13 +14,4 @@ type Users struct{
 	Password	string
 	BirthDay	time.Time 
 	Profilepic	string	`gorm:"type:longtext"`
-
-	GenderID	uint	`json:"gender_id"`	
-	Gender		*Genders `gorm:"foreignKey: gender_id" json:"gender"`
-	
-	UserRoleID	uint	`gorm:"userrole_id"`
-	UserRole	*UserRoles	`gorm:"foreignKey: userrole_id" `
-
-	
-
 }
